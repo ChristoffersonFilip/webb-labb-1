984ac1ffd2d5e111201517e4e5b2216fa0186483
Filip <filip.christofferson@gmail.com>
2019-03-29 15:25:43 +0100

    Get innerHTML working, add score function

--- Labb-1.js
+++ Labb-1.js
@@ -1,7 +1,19 @@
+var correct = 0;
+var incorrect = 0;
+var question = "none";
+var choice = "none";
+var answer = "none";
+
 
 function loadQuestions(){
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', 'https://opentdb.com/api.php?amount=3&type=multiple', true);
+    
+
+    //used to not have to repeat document.getElementById constantly
+    function get(x){
+        return document.getElementById(x);
+    }
 
     httpRequest.onload = function(){
         if(this.status == 200){
@@ -23,21 +35,28 @@ function loadQuestions(){
             var question3_difficulty = question[2].difficulty
             var question3_category = question[2].category;
 
-            console.log(question1_category)
-            console.log(question1_difficulty);
-            console.log(question1);
-            console.log(question1_alternatives);
-            console.log(question2_category);
-            console.log(question2_difficulty);
-            console.log(question2);
-            console.log(question2_alternatives);
-            console.log(question3_category);
-            console.log(question3_difficulty);
-            console.log(question3);
-            console.log(question3_alternatives);
+
+            get('question').innerHTML = "Question: " +question1;
+           // get('question1-answers').innerHTML = "Answer Options: "+question1_alternatives;
+            get('question2').innerHTML = "Question 2: " +question2;
+            get('question2-answers').innerHTML = "Answer Options: "+question2_alternatives;
+            get('question3').innerHTML = "Question 3: " +question3;
+            get('question3-answers').innerHTML = "Answer Options: "+question3_alternatives;
+
+            get('question1-answers').innerHTML += "<input type='radio' name='choices' value='A'>"+question1_alternatives+"<button onclick='checkAnswer()'>";
 
         }
     }
     httpRequest.send();
+}
 
+var score = function(){
+    if(choice == question[pos].correct_answer){
+        correct++;
+    }
+
+}
+
+function correctAnswers(){
+    
 }
