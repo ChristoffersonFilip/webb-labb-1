89783d3722a0e2244d8a3b2cb89077f22fb364bb
Filip <filip.christofferson@gmail.com>
2019-04-05 09:21:25 +0200

    Changed file locations

--- javascript/Labb-1.js
+++ javascript/Labb-1.js
@@ -4,6 +4,7 @@ var question;
 var choice = "none";
 var answer = "none";
 
+//document.getElementById("submitTest").addEventListener("click", sumbitAnswers(), displayResults());
 //used to not have to repeat document.getElementById constantly
 function get(x){
         return document.getElementById(x);
@@ -14,28 +15,19 @@ function loadQuestions(){
     httpRequest.open('GET', 'https://opentdb.com/api.php?amount=3&type=multiple', true);
     
 
-    
-    
-
     httpRequest.onload = function(){
         if(this.status == 200){
             question = JSON.parse(this.responseText).results;
-            console.log(question);
-
+        
             var question1 = question[0].question;
-            var question1_alternatives =  [question[0].incorrect_answers[0], question[0].incorrect_answers[1], question[0].incorrect_answers[2], question[0].correct_answer];
-            var question1_difficulty = question[0].difficulty;
-            var question1_category = question[0].category;
+            var question1_alternatives =  [question[0].incorrect_answers[0], question[0].incorrect_answers[1], question[0].incorrect_answers[2], question[0].correct_answer];    
 
             var question2 = question[1].question;
             var question2_alternatives = [question[1].incorrect_answers[0], question[1].incorrect_answers[1], question[1].incorrect_answers[2], question[1].correct_answer];
-            var question2_difficulty = question[1].difficulty;
-            var question2_category = question[1].category;
 
             var question3 = question[2].question;
             var question3_alternatives = [question[2].incorrect_answers[0], question[2].incorrect_answers[1], question[2].incorrect_answers[2], question[2].correct_answer];
-            var question3_difficulty = question[2].difficulty
-            var question3_category = question[2].category;
+            
 
             //saves the newly shuffled answers into the question variables
             question1_alternatives = shuffleOptions(question1_alternatives);
